2b28e84c37a76f04243332abbfa854cb84c961c9
CharlottePrimiceri <115116451+CharlottePrimiceri@users.noreply.github.com>
2024-07-18 16:43:45 +0200

    Add files via upload

---
+++ 04-Calibration/tricyclecalibration.m
@@ -0,0 +1,10 @@
+function delta_pose_T=predictFront_Tractor_Tricycle(traction_angular_offset,
+                                                 measured_steering_angle, x)
+    tr = 
+    tl =
+    dx = 
+    dy = 
+     
+    delta_pose_T=[dx; dy; dth]
+
+endfunction
