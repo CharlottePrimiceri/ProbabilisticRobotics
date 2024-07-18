581bb7fef75a7f86e094afd6f8b236808c1a2268
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-07-18 18:16:03 +0200

    display

--- 04-Calibration/tricyclecalibration.m
+++ 04-Calibration/tricyclecalibration.m
@@ -8,3 +8,12 @@ function delta_pose_T=predictFront_Tractor_Tricycle(traction_angular_offset,
     delta_pose_T=[dx; dy; dth]
 
 endfunction
+
+disp('2D position of sensor w.r.t. base link');
+S = function1();
+disp(S);
+pause(1);
+
+disp('kinematic parameters');
+P = function2();
+disp(P);
