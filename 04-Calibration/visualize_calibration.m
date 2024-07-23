1e9dd836c6e0ac798c86a14972144008e98282f2
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-07-23 15:36:20 +0200

    files division

---
+++ 04-Calibration/visualize_calibration.m
@@ -0,0 +1,11 @@
+#put also there the ground truth trajectory?
+#show the uncalibrated and calibrated odometry? 
+
+disp('2D position of sensor w.r.t. base link');
+S = function1();
+disp(S);
+pause(1);
+
+disp('kinematic parameters');
+P = function2();
+disp(P);
