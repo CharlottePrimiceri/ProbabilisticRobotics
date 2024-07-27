d1cfe4dad6508479ac6513f235e57739788bb933
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-07-27 18:43:26 +0200

    modify function

--- 04-Calibration/calibrated_odometry.m
+++ 04-Calibration/calibrated_odometry.m
@@ -1,25 +1,14 @@
-#U: matrix contains odometry 
+#U: matrix contains odometry, itprovides the incremental changes in the robot's position and orientation at each time step.
 
-#defining function to compute trajectory
+#defining function to compute trajectory T (so the absolute position and orientation)
 function T= odometry_trajectory()
          
          T=zeros(size(U,1), 3);
          current_T=v2t(zeros(1,3));
          for i=1:size(U,1),
-            
-
-endfunction
-
-
-
-
-function delta_pose_T=predictFront_Tractor_Tricycle(traction_angular_offset,
-                                                 measured_steering_angle, x)
-    tr = 
-    tl =
-    dx = 
-    dy = 
-    dth = 
-    delta_pose_T=[dx; dy; dth]
+                u=U(i,1:3)';
+                current_T*=v2t(u);
+		        T(i,1:3)=t2v(current_T)';
+	     end
 
 endfunction
