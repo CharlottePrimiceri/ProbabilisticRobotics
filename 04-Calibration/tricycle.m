6b75612a6a3ce562d6c2d5bbe23bc81343c070e7
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-07-27 18:42:45 +0200

    add function

---
+++ 04-Calibration/tricycle.m
@@ -0,0 +1,10 @@
+function delta_pose_T=predictFront_Tractor_Tricycle(traction_angular_offset,
+                                                 measured_steering_angle, x)
+    tr = 
+    tl =
+    dx = 
+    dy = 
+    dth = 
+    delta_pose_T=[dx; dy; dth]
+
+endfunction
