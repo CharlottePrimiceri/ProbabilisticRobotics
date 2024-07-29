7e69e327145909fcd66ab32cf841ec536ee1027e
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-07-29 16:10:52 +0200

    n_lines

--- 04-Calibration/visualize_calibration.m
+++ 04-Calibration/visualize_calibration.m
@@ -6,43 +6,33 @@ clc
 #prova
 #import 2d geometry utils
 source "../tools/utilities/geometry_helpers_2d.m"
-
+#source "../04-Calibration/calibrated_odometry.m"
 disp('loading the matrix');
-% Read the file content
-fid = fopen('dataset.txt');
-if fid == -1
+
+% Read dataset.... transform then into function
+#function []=reaf_file(path)
+file = fopen('dataset.txt', 'r');
+if file == -1
     error('Failed to open the file.');
 end
-% Initialize an empty array to store the model_pose values
-model_pose_values = [];
-
-% Read the file line by line
-while ~feof(fid)
-    line = fgetl(fid);
-    
-    % Use a regular expression to extract the model_pose values
-    pattern = 'model_pose:\s*([^\s]+)\s*([^\s]+)\s*([^\s]+)';
-    tokens = regexp(line, pattern, 'tokens');
-    
-    % If model_pose values are found, convert them to numbers and store them
-
-    if ~isempty(tokens)
-        values = str2double(tokens{1});
-        model_pose_values = [model_pose_values; values]; % Append to the array
-    end
+#don't include the commented rows
+for i = 1:8
+    fgetl(file);
 end
-% Close the file
-fclose(fid);
+#reds text into a cell array of strings, each line is a cell
+dataset = textscan(file, '%s', 'Delimiter', '\n', 'MultipleDelimsAsOne', 1);
 
-% Display the extracted model_pose values
-disp('Model Pose Values:');
-disp(model_pose_values);
-#a
+#dataset = textscan(file, '%s: %f %s: %d %d %s: %f %f %f %s: %f %f %f', 'Delimiter', ' ');
+lines = dataset{1};
+n_lines = size(lines)(1);
+disp(n_lines);
+
+
 %Z=load('/home/charlotte/Documents/ProbabilisticRobotics/04-Calibration/dataset.txt');
 %addpath('/home/charlotte/Documents/ProbabilisticRobotics/04-Calibration');
 
-%compute the ground truth trajectory
-%TrueTrajectory=compute_odometry_trajectory(Z(:,1:3));
+#compute the ground truth trajectory
+#TrueTrajectory=odometry_trajectory(Z(:,1:3));
 
 %disp('2D position of sensor w.r.t. base link');
 %S = function1();
