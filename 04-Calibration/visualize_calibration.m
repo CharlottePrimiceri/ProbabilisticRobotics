2e78465a84e5935a579ff40646b2d624b7a8dae4
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-07-29 16:32:33 +0200

    read odometry parameters

--- 04-Calibration/visualize_calibration.m
+++ 04-Calibration/visualize_calibration.m
@@ -3,29 +3,33 @@
 close all
 clear
 clc
-#prova
 #import 2d geometry utils
 source "../tools/utilities/geometry_helpers_2d.m"
 #source "../04-Calibration/calibrated_odometry.m"
 disp('loading the matrix');
 
 % Read dataset.... transform then into function
-#function []=reaf_file(path)
+#function []=read_file(path)
 file = fopen('dataset.txt', 'r');
+
 if file == -1
     error('Failed to open the file.');
 end
-#don't include the commented rows
-for i = 1:8
-    fgetl(file);
-end
-#reds text into a cell array of strings, each line is a cell
-dataset = textscan(file, '%s', 'Delimiter', '\n', 'MultipleDelimsAsOne', 1);
 
-#dataset = textscan(file, '%s: %f %s: %d %d %s: %f %f %f %s: %f %f %f', 'Delimiter', ' ');
+#reads text into a cell array of strings, each line is a cell
+dataset = textscan(file, '%s', 'Delimiter', '\n', 'MultipleDelimsAsOne', 1);
 lines = dataset{1};
 n_lines = size(lines)(1);
-disp(n_lines);
+#disp(n_lines);
+
+#odometry matrix
+odometry = zeros(n_lines-3, 3);
+#from 9 so it doesn't include the first 8 rows
+for i = 9:n_lines
+    line = lines{i};
+    line = textscan(line, 'time: %f ticks: %d %d model_pose: %f %f %f tracker_pose: %f %f %f', 'Delimiter', ' ', 'MultipleDelimsAsOne', 1);
+    odometry(i-3,:) = cell2mat(line(4:6));
+end    
 
 
 %Z=load('/home/charlotte/Documents/ProbabilisticRobotics/04-Calibration/dataset.txt');
