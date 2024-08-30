090c3fcfdd167e86f1065784a900136f1ecd3293
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-08-30 12:29:05 +0200

    add uncalibrated odometry visualization

--- 04-Calibration/visualize_calibration.m
+++ 04-Calibration/visualize_calibration.m
@@ -22,27 +22,31 @@ function odometry=read_odometry(path)
         #disp(n_lines);
 
         #odometry matrix, without first 8 lines
-        odometry = zeros(n_lines-8, 3);
+        odometry = zeros(n_lines-8, 6);
         #from 9 so it doesn't include the first 8 rows
         for i = 9:n_lines
             line = lines{i};
             line = textscan(line, 'time: %f ticks: %d %d model_pose: %f %f %f tracker_pose: %f %f %f', 'Delimiter', ' ', 'MultipleDelimsAsOne', 1);
-            odometry(i-8,:) = cell2mat(line(4:6));
+            odometry(i-8,:) = cell2mat(line(4:9)); #here modified 4:6
         end    
 endfunction
 
 U = read_odometry(path);
+disp(U)
 #compute the ground truth trajectory
-T = odometry_trajectory(U(:,1:3));
+T = odometry_trajectory(U(:,4:6));
 disp('ground truth');
 hold on;
 plot(T(:,1),T(:,2), 'r-', 'linewidth', 2);
 pause(10);
 
 
-#compute the ground truth trajectory
-#TrueTrajectory=odometry_trajectory(Z(:,1:3));
-
+#compute the uncalibrated odometry
+uncalibrated_odometry = odometry_trajectory(U(:,1:3));
+disp('Uncalibrated Odometry');
+hold on;
+plot(uncalibrated_odometry(:,1), uncalibrated_odometry(:,2), 'b-', 'linewidth', 2);
+pause(10);
 %disp('2D position of sensor w.r.t. base link');
 %S = function1();
 %disp(S);
