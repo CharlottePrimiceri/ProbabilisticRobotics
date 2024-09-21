77732bec1dd9cc129d8b34a0829e2b5ce94aa101
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-09-21 11:25:13 +0200

    comment_kinmodel

--- 04-Calibration/tricycle.m
+++ 04-Calibration/tricycle.m
@@ -1,10 +1,18 @@
 function delta_pose_T=predictFront_Tractor_Tricycle(traction_angular_offset,
-                                                 measured_steering_angle, x)
-    tr = 
-    tl =
-    dx = 
-    dy = 
-    dth = 
-    delta_pose_T=[dx; dy; dth]
+                                                 measured_steering_angle, initial_state)
+    
+    #i can find the steering angle but not the actual front displacement bc i don't have the radius
+    #should i use the classic kinematic model by imposing some initial value on the front wheel? 
+
+    traction_front = ;
+    steer_angle= ;
+    delta = ;
+    S = [];
+    C = [];
+    dx = delta * polyval(S,dth);
+    dy = delta * polyval(C,dth);
+
+    delta_pose_T=[dx; dy; dth];
+
 
 endfunction
