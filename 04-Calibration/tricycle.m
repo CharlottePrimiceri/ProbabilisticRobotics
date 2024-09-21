0cf3d13ad7d0df603aa4419e25610b9cac164737
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-09-21 11:52:39 +0200

    update

--- 04-Calibration/tricycle.m
+++ 04-Calibration/tricycle.m
@@ -3,7 +3,8 @@ function delta_pose_T=predictFront_Tractor_Tricycle(traction_angular_offset,
     
     #i can find the steering angle but not the actual front displacement bc i don't have the radius
     #should i use the classic kinematic model by imposing some initial value on the front wheel? 
-
+    #but i still can find the displacement by (current_tick - previous_tick)kt
+    #i can put the computation for the overflow directly there
     traction_front = ;
     steer_angle= ;
     delta = ;
