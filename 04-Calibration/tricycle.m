a3a86f46115328086811371a02fabd4bfb15cb6c
CharlottePrimiceri <primiceri2021543@studenti.uniroma1.it>
2024-09-24 01:05:27 +0200

    add the variation of theta based on steering angle and front wheel displacement

--- 04-Calibration/tricycle.m
+++ 04-Calibration/tricycle.m
@@ -6,6 +6,7 @@ function delta_pose_T=predictFront_Tractor_Tryìicycle(traction_incremental_tick
     ticks_to_meters=kin_parameters(2);
     traction_max = max_enc_values(2);
     ticks_to_radians = kin_parameters(1);
+    axis_lenght = kin_parameters(3);
     #i can find the steering angle but not the actual front displacement bc i don't have the radius
     #should i use the classic kinematic model by imposing some initial value on the front wheel? 
     #but i still can find the displacement by (current_tick - previous_tick)kt
@@ -16,15 +17,23 @@ function delta_pose_T=predictFront_Tractor_Tryìicycle(traction_incremental_tick
     traction_front = traction_incremental_ticks * (ticks_to_meters / traction_max);
     
     # (ticks_to_radians *2*pi / steer_max) is the value of radians (converted from revolution to
-    # radians with a factor 2pi)  corresponds to one single tick
+    # radians with a factor 2pi)  corresponds to one single tick in case of positive angles
     if ticks_to_radians < (steer_max/2)
       steer_angle = steering_ticks * (ticks_to_radians *2*pi / steer_max) + steer_offset;
     else  
+    # considering negative angles
       steer_angle = -steering_ticks * [(steer_max-ticks_to_radians) *2*pi / steer_max] + steer_offset;
     endif
+    
+    # drawing the model of the tricycle we obtain that relationship
+    back_wheel_displacement = traction_front*cos(steer_angle);
 
+    dth = back_wheel_displacement*sin(steer_angle)/axis_lenght;
+    
+    # need to find S and C
     S = [];
     C = [];
+
     dx = delta * polyval(S,dth);
     dy = delta * polyval(C,dth);
 
@@ -34,5 +43,9 @@ function delta_pose_T=predictFront_Tractor_Tryìicycle(traction_incremental_tick
 endfunction
 
 function laser_pose_uncalibrated = laser_pose_uncalibrated(kin_parameters, U, tricycle)
+         
+        for i = 1:n
+            
+        endfor
 
 endfunction
